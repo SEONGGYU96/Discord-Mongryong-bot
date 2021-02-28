e3dc934f8a9c0de7e7ccec34edff7c740bbfcfd3
KIM SEONGGYU <link5658@gmail.com>
2021-02-28 11:42:10 +0900

    Initial commit

---
+++ src/main/kotlin/com/gugyu/mongryongbot/MongryongBotApplication.kt
@@ -0,0 +1,11 @@
+package com.gugyu.mongryongbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongryongBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<MongryongBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/gugyu/mongryongbot/MongryongBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gugyu.mongryongbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongryongBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
